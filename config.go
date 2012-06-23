3f45992ad5427d516936d913e2ef75996fa27b37
Ingo Oeser <nightlyone@googlemail.com>
2012-06-23 17:13:34 +0200

    go fmt

--- config.go
+++ config.go
@@ -1,19 +1,19 @@
 package socketio
 
 type Config struct {
-	CloseTimeout         int64
-	HeartbeatInterval    int64
-	HeartbeatTimeout     int64
-	PollingTimeout       int64
-	Transports           []*Transport
-	WriteTimeout         int64
+	CloseTimeout      int64
+	HeartbeatInterval int64
+	HeartbeatTimeout  int64
+	PollingTimeout    int64
+	Transports        []*Transport
+	WriteTimeout      int64
 }
 
 var DefaultConfig = Config{
-	CloseTimeout:         25e9,
-	HeartbeatInterval:    15e9,
-	HeartbeatTimeout:     10e9,
-	PollingTimeout:       20e9,
-	Transports:           DefaultTransports,
-	WriteTimeout:         5e9,
+	CloseTimeout:      25e9,
+	HeartbeatInterval: 15e9,
+	HeartbeatTimeout:  10e9,
+	PollingTimeout:    20e9,
+	Transports:        DefaultTransports,
+	WriteTimeout:      5e9,
 }
